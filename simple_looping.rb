03f13e6f6f8e2abaf72b3e1d30b04bf654e5eb91
cwoolnoughchamorro <cwoolnoughchamorro@googlemail.com>
2018-09-19 08:59:53 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -24,7 +24,8 @@ end
 def until_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   count = 0
-  until
+  until count == number_of_times
+   put phrase
 
 end
 
