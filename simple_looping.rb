e1006a594b1231f86e1d2684415a08ebcad69f0d
cwoolnoughchamorro <cwoolnoughchamorro@googlemail.com>
2018-09-19 08:52:51 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -32,7 +32,7 @@ def for_iterator(number_of_times)
   count = 1..number_of_times
   phrase = "Welcome to Flatiron School's Web Development Course!"
   for number in number_of_times
-  puts phrase
+    puts phrase
   end
 end
 
